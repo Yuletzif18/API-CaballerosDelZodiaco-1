42f1a26dace87b9495b7e923e2f40d0a05aa99c6
Yuletzif18 <132315468+Yuletzif18@users.noreply.github.com>
2025-10-31 13:48:59 -0500

    Refactor caballeroRoutes to remove file upload handling
    Removed multer configuration and updated routes to handle requests without file uploads.

--- saintseiya-backend/routes/caballeroRoutes.js
+++ saintseiya-backend/routes/caballeroRoutes.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const caballeroController = require('../controllers/caballeroController');
-const multer = require('multer');
-const path = require('path');
 
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, path.join(__dirname, '../uploads'));
-	},
-	filename: function (req, file, cb) {
-		cb(null, Date.now() + '-' + file.originalname);
-	}
-});
-const upload = multer({ storage });
-
+// Rutas limpias, solo JSON
 router.delete('/caballero/:nombre', caballeroController.eliminarPorNombre);
-router.put('/caballero/:nombre', upload.single('imagen'), caballeroController.modificarPorNombre);
-router.post('/caballero', upload.single('imagen'), caballeroController.crearCaballeroYBatalla);
+router.put('/caballero/:nombre', caballeroController.modificarPorNombre);
+router.post('/caballero', caballeroController.crearCaballeroYBatalla);
 router.get('/caballero/:nombre', caballeroController.consultarPorNombre);
 router.get('/caballeros', caballeroController.listarTodos);
 
